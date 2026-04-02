d06ba125d1dca13ba46ce81f76b12deccc8788b4
AbhinavKumar010 <michaelabhinavthecloud@gmail.com>
2026-04-02 23:16:38 +0530

    bhjk

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./styles/LandingPage.css";
+import "./styles/Landingpage.css";
 
 function LandingPage() {
   const navigate = useNavigate();
